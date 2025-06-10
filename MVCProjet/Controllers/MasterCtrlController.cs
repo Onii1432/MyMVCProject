143c3a795033a41595be6e8bb4cb7533dff0acf4
sneha_gharat <42944127+snehagharat@users.noreply.github.com>
2025-06-10 15:06:11 +0530

    one more method added

--- MVCProjet/Controllers/MasterCtrlController.cs
+++ MVCProjet/Controllers/MasterCtrlController.cs
@@ -24,5 +24,12 @@ namespace MVCProjet.Controllers
             public int ID { get; set; }
 
         }
+
+        public class ListModel2
+        {
+
+            public int ID { get; set; }
+
+        }
     }
 }
