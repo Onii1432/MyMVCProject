022c4e7eed65be5ebeb9a48410b302f5ed0e9afe
sneha_gharat <42944127+snehagharat@users.noreply.github.com>
2025-06-10 13:44:22 +0530

    Add Ctrl Master

---
+++ MVCProjet/Controllers/MasterCtrlController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProjet.Controllers
+{
+    public class MasterCtrlController : Controller
+    {
+        // GET: MasterCtrl
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
